4070208b1611f7a998dce495abf15194a4dff6b2
Devikakjijith <161475876+Devikakjijith@users.noreply.github.com>
2026-09-15 10:56:50 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 MYSQL_HOST = os.environ.get("MYSQL_HOST", "localhost")
 MYSQL_PORT = int(os.environ.get("MYSQL_PORT", 3306))
 MYSQL_USER = os.environ.get("MYSQL_USER", "root")
-MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "devika@2004")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "your_sql_password")
 MYSQL_DB = os.environ.get("MYSQL_DB", "library_mng_system")
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "change-this-secret-key-in-production")
